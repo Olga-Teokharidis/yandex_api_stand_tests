146b857bd3ab2d7afb2f30d7a612448cae921fa4
Olga Teokharidis <o.teokharidis@gmail.com>
2024-09-12 11:18:06 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,11 @@
+URL_SERVICE = "https://7d0cb3c9-48ac-4697-bf5b-7066b93aa9dc.serverhub.praktikum-services.ru"
+
+DOC_PATH = "/docs/"
+
+LOG_MAIN_PATH = "/api/logs/main/"
+
+CREATE_USER_PATH = "/api/v1/users/"
+
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
+
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
